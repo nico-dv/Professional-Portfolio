9848bc4e41bbd80022390a22bd9d02be0194a361
nico-dv <vv.nicov@gmail.com>
2022-07-14 19:11:31 +0100

    added main.py

---
+++ Text to Morse/main.py
@@ -0,0 +1,33 @@
+
+MORSE_CODE = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+print("\\** This is a script that translates text to morse code **//")
+message = input("Insert your text:\n").upper()
+
+new_list = []
+
+for letter in message:
+    if letter in MORSE_CODE:
+        new_list.append(MORSE_CODE[letter])
+
+
+print(new_list)   
+    
+    
+    # for key,value in MORSE_CODE.items():
+    #     if index.upper() == key:
+    #         print(value)
