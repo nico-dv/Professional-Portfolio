2a5dffa83c11f97901ee27f4eb65362ce6944978
nico-dv <vv.nicov@gmail.com>
2022-07-19 23:49:53 +0100

    added audio stream access and audio play

--- Audiobook-converter/main.py
+++ Audiobook-converter/main.py
@@ -20,3 +20,25 @@ except (BotoCoreError, ClientError) as error:
     sys.exit(-1)
 
 
+if "AudioStream" in response:
+        with closing(response["AudioStream"]) as stream:
+           output = os.path.join(gettempdir(), "speech.mp3")
+
+           try:
+                with open(output, "wb") as file:
+                   file.write(stream.read())
+           except IOError as error:
+              print(error)
+              sys.exit(-1)
+else:
+   
+    print("Could not stream audio")
+    sys.exit(-1)
+
+
+if sys.platform == "win32":
+    os.startfile(output)
+else:
+    
+    opener = "open" if sys.platform == "darwin" else "xdg-open"
+    subprocess.call([opener, output])
