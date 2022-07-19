487fa380a1b5c46f0bfc6c8a231eb3e6d2b78070
nico-dv <vv.nicov@gmail.com>
2022-07-19 23:41:15 +0100

    added pdf converter

---
+++ Audiobook-converter/pdf.py
@@ -0,0 +1,10 @@
+from PyPDF2 import PdfReader
+
+
+def pdf_converter():
+    reader = PdfReader("/home/nico/Desktop/sample.pdf")
+    page = reader.pages[0]
+    return page.extract_text()
+    
+
+
